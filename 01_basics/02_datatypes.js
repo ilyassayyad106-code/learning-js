a0e8aeb5b7ab93111e0d0bc5697b0af5b8459561
ilyassayyad106-code <ilyassayyad106@gmail.com>
2025-11-10 15:58:03 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+// alert (3+3) // we are using nodejs, not broweser. 
+
+//number    => 12345
+//bigint    
+//string    => " "
+//boolean   => true/false
+//null      => standallone value
+//undefined => 
+//symbol    => unique
+
+//obect     =>
+
+    console.log(typeof undefined); //undefined
+    console.log(typeof null); //object 
