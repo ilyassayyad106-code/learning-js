f8e63a905a00b93dc5dd773bdca0c43ec56d865c
ilyassayyad106-code <ilyassayyad106@gmail.com>
2025-11-10 18:44:17 +0000

    conversion and operations in data types

--- 01_basics/03_conversionORoprations.js
+++ 01_basics/03_conversionORoprations.js
@@ -1,53 +1,88 @@
-let score = 34
+// //let score = 34
 
-console.log(typeof score);      //number
-console.log(typeof(score));     //number
+// console.log(typeof score);      //number
+// console.log(typeof(score));     //number
 
-let Score = "34abc"
+// //let Score = "34abc"
 
-console.log(typeof Score);      //string
-console.log(typeof(Score));     //string
+// console.log(typeof Score);      //string
+// console.log(typeof(Score));     //string
 
-let valueInNumber = Number(Score);
+// //let valueInNumber = Number(Score);
 
-console.log(typeof valueInNumber);  //number
-console.log( valueInNumber);        //NaN 
+// console.log(typeof valueInNumber);  //number
+// console.log( valueInNumber);        //NaN 
 
-let value1 = NaN;
-console.log(value1);  //NaN
+// //let value1 = NaN;
+// console.log(value1);  //NaN
 
-let value2 = undefined;
-console.log(value2); //undefined
+// //let value2 = undefined;
+// console.log(value2); //undefined
 
-let valueInNumber2 = Number(value2);
-console.log(valueInNumber2);  // NaN
+// //let valueInNumber2 = Number(value2);
+// console.log(valueInNumber2);  // NaN
 
-let value3 = null;
-console.log(value3);  //null
+// //let value3 = null;
+// console.log(value3);  //null
 
-let valueInNumber3 = Number(value3);
-console.log(valueInNumber3);  // 0
+// //let valueInNumber3 = Number(value3);
+// console.log(valueInNumber3);  // 0
 
-let value4 = true;
-console.log(value4);  //true
+// //let value4 = true;
+// console.log(value4);  //true
 
-let valueInNumber4 =Number(value4);
-console.log(valueInNumber4);  //1
+// //let valueInNumber4 =Number(value4);
+// console.log(valueInNumber4);  //1
 
-// "34" => 34
-//"34abc"=>NaN
-//true => 1 ; false => 0
+// // "34" => 34
+// //"34abc"=>NaN
+// //true => 1 ; false => 0
 
-let isLoggedIn = 1;
-let booleanisLoggedIn = Boolean(isLoggedIn);
-console.log(booleanisLoggedIn);   //true
+// //let isLoggedIn = 1;
+// //let booleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanisLoggedIn);   //true
 
-// 1 => true ; 0 => false
-// " " => false
-//"ilyas" => true
+// // 1 => true ; 0 => false
+// // " " => false
+// //"ilyas" => true
 
-let someNumber = 55;
+// //let someNumber = 55;
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);   //55
-console.log(typeof stringNumber); //string
+// //let stringNumber = String(someNumber);
+// console.log(stringNumber);   //55
+// console.log(typeof stringNumber); //string
+
+
+// ***********************************Oprations*******************************************
+   
+let value = 4;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(4+4);
+// console.log(4-4);
+// console.log(4*4);    //basc math operations
+// console.log(4**5);
+// console.log(4/3);
+// console.log(4%6);
+
+let str1 ="hello";
+let str2 =" ilyas";
+let str3=str1 + str2;
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); // if string is first then all conversion will be treat as string
+// console.log(1 + 2 + "2");// if string is at last then addition or other operations will be happen first
+
+// console.log(+true);
+// console.log(+"");    // avoid its just for understanding . 
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2; //avoid writing code like this 
+
+let gameCounter = 100;
+++gameCounter; //we used ++gamecounter "prefix" study pre and profix from mdn as homework
+console.log(gameCounter);
+
