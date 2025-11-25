0974a964d4b214875d51f89af320d4e215365230
ilyassayyad106-code <ilyassayyad106@gmail.com>
2025-11-25 16:47:22 +0000

    Stack and Heap

--- 01_basics/DataTypes-Summery.js
+++ 01_basics/DataTypes-Summery.js
@@ -6,43 +6,68 @@ types: 7 types
 
 */
 
-const score = 100;
-const scoreValue = 100.3;
+// const score = 100;
+// const scoreValue = 100.3;
 
-const isLoggedIn = false;
-const outsideTemp= null;
+// const isLoggedIn = false;
+// const outsideTemp= null;
 
-let userEmail;
+// let userEmail;
 
-const id =Symbol('123');
-const id2=Symbol('123');
+// const id =Symbol('123');
+// const id2=Symbol('123');
 
-console.log(id === id2);  //false
+// console.log(id === id2);  //false
 
-// const bigNumber = 123456789n
-/*
+// // const bigNumber = 123456789n
+// /*
 
-   #Refrence(Non-primitive): Call by
+//    #Refrence(Non-primitive): Call by
  
-types: 3 types
-1. Arrays, 2.Objects, 3.Functions.
+// types: 3 types
+// 1. Arrays, 2.Objects, 3.Functions.
 
-*/
+// */
 
-const heros = ["Ironman", "Thor", "C.America" ] // []=arrays
+// const heros = ["Ironman", "Thor", "C.America" ] // []=arrays
 
-let myObj={    // {}=Object
-   name: "ilyas",
-   age:25,
+// let myObj={    // {}=Object
+//    name: "ilyas",
+//    age:25,
+// }
+
+// const myFuction = function(){
+//    console.log("Hello world ");
+// }
+
+// // How to find data type:
+// console.log(typeof bigNumber);    //undefined
+// console.log(typeof outsideTemp);  //object
+// console.log(typeof scoreValue);   //number
+// console.log(typeof myFuction);   //function
+// console.log(typeof id2);         //symbol
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack (Primitive), Heap (Non-Primitive)
+
+let myName = "ilyas"
+
+let anotherName = "myName"
+anotherName = "umar"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+   email: "user@.com",
+   upi: "user@ybl"
 }
 
-const myFuction = function(){
-   console.log("Hello world ");
-}
+let userTwo = userOne
 
-// How to find data type:
-console.log(typeof bigNumber);    //undefined
-console.log(typeof outsideTemp);  //object
-console.log(typeof scoreValue);   //number
-console.log(typeof myFuction);   //function
-console.log(typeof id2);         //symbol
+userTwo.email = "ilyas@.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
