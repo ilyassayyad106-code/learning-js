38264892572189e564a79aafd1bf6f1ce12b0d5d
ilyassayyad106-code <ilyassayyad106@gmail.com>
2025-12-02 16:45:49 +0000

    methods in array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,6 +1,35 @@
 // array
 
 const myArr = [0, 1, 2, 3, 4, 5, "ilyas"]
-console.log(myArr[1]);
+// console.log(myArr[1]);
 
 // Array methods
+
+// myArr.push(4)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)  // add new value at start
+// myArr.shift()    // remove first value 
+
+// console.log(myArr.includes(9)); // it'll check if we have this value in our arr.in boolean
+// console.log(myArr.indexOf(4)); //it'll show in which index asked value is.is its not -1.
+
+// const newArr = myArr.join() // our arr will be changed in string and binded it.
+
+// console.log(myArr);  // [0, 1, 2, 3, 4, 5, `ilyas`]
+// console.log(newArr);//  0,1,2,3,4,5,ilyas
+
+// Slice , Splice
+
+console.log("A", myArr);
+
+const myna1 = myArr.slice(1,3) //slice doesn't manupulate original arr
+console.log("slice", myna1);
+
+console.log("B", myArr);
+
+const myna2 = myArr.splice(1,3)  // splice manupulate original arr
+console.log("c", myArr);
+
+console.log("splice", myna2);
