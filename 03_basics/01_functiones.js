30a07414c4ea47ec6e4f315f0e82b734da288c9d
ilyassayyad106-code <ilyassayyad106@gmail.com>
2025-12-14 18:21:49 +0000

    function start

---
+++ 03_basics/01_functiones.js
@@ -0,0 +1,27 @@
+// normal code: this is how we print something 
+
+// console.log("I");
+// console.log("L");
+// console.log("Y");
+// console.log("A");
+// console.log("S");
+
+//Functions:
+
+function sayMyName(){
+    
+ console.log("I");
+ console.log("L");
+ console.log("Y");
+ console.log("A");
+ console.log("S");
+}
+// sayMyName()
+
+function addTwoNumbers(number1, number2){//when we create defn.of functions,(xyz1, xyz2)are called parameters 
+    console.log(number1 + number2);
+}
+//   addTwoNumbers(7 , 8) //when we call functions (xyz,xyz)are called arguments
+//   addTwoNumbers(7, "2") // 72 as a string
+//   addTwoNumbers(7 , "b" ) // 7b
+  addTwoNumbers(7, null) //7
