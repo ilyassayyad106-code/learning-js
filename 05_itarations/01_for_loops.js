39a4c1ad846176b2d95be53a50b26de4ff04a540
ilyassayyad106-code <ilyassayyad106@gmail.com>
2025-12-31 19:17:26 +0000

    for loop

---
+++ 05_itarations/01_for_loops.js
@@ -0,0 +1,18 @@
+
+// for loops:
+
+for(let index=0; index<=10; index++){
+    const element = index
+    console.log(element);
+    
+}
+
+//lets use if statement inside for loop
+
+for( let i=0; i<=15; i++){
+    const element = i
+    if(element==7){
+        console.log("7 is cool number");
+       }
+       console.log(element)
+}
