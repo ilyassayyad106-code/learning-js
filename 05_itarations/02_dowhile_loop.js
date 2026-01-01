d6cd12e9b4e8119ce6c9634bacb004940e314956
ilyassayyad106-code <ilyassayyad106@gmail.com>
2026-01-01 18:55:48 +0000

    while and do while loop

---
+++ 05_itarations/02_dowhile_loop.js
@@ -0,0 +1,29 @@
+
+// while loop:
+
+let index = 0
+while (index <= 10){
+    console.log(`value of index is ${index}`);
+    index = index + 2
+     
+    
+}
+
+//with array
+
+let myArray = [`dr.strange`, `spiderman`, `hulk`]
+let arr=0
+while(arr<myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr=arr+1
+}
+
+//do while loop
+
+let score = 1
+
+do{
+    console.log(`value is ${score}`);
+    score++
+    
+}while(score<=5)
