ed1335398917f85966dec440ec5721f08f80a3d4
ilyassayyad106-code <ilyassayyad106@gmail.com>
2026-01-01 16:07:33 +0000

    nested loops and tables

--- 05_itarations/01_for_loops.js
+++ 05_itarations/01_for_loops.js
@@ -16,3 +16,27 @@ for( let i=0; i<=15; i++){
        }
        console.log(element)
 }
+
+// nested loop and lets print tables:
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`value ${i}`);
+
+     for (let j = 1; j <= 10; j++) {
+        
+    console.log( i + ` * ` +  j  +  ` = `  + i*j); //table
+           
+     }
+    
+}
+
+//in array:
+
+let myArr = ["thor" , "iron man" , "captain"]
+
+for (let index = 0; index < myArr.length; index++) {
+    const element = myArr[index];
+    console.log(element);
+    
+    
+}
